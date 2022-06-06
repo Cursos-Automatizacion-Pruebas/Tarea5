553125dd4c2579bdcc01ea1d24e5b3e033127aaa
Juan De Dios <jbaudazio@gmail.com>
2022-06-06 01:25:11 -0500

    ejercicio 5 terminado y 4 en curso

--- src/test/java/RemoveElementsTests_falta_todo.java
+++ src/test/java/RemoveElementsTests_falta_todo.java
@@ -2,11 +2,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.Logs;
+
 public class RemoveElementsTests_falta_todo {
     private WebDriver driver;
     private final Logs log = new Logs();
@@ -25,7 +25,7 @@ public class RemoveElementsTests_falta_todo {
     }
 
     @Test
-    public void theInternetAddRemoveElementsTest() {
+    public void theInternetAddRemoveElementsTest() throws InterruptedException {
         var url = " http://the-internet.herokuapp.com/add_remove_elements/";
 
         log.info("Voy a la pagina: " + url);
@@ -38,7 +38,14 @@ public class RemoveElementsTests_falta_todo {
             log.error(e.getLocalizedMessage());
         }
 
+        var addElementBotonLocator = By.tagName("button");
+        var listaBotones = driver.findElements(addElementBotonLocator);
 
+        log.info("Haciendo click en boton add Element");
+        for (int i = 0; i < 10; i++) {
+            listaBotones.get(0).click();
+            //listaBotones.add(get(i));
+        }
     }
 
     @AfterMethod
